4d07e3d9a95659deb3bd3a81512c2aba24dd7ed4
ram023 <102957944+ram023@users.noreply.github.com>
2022-04-04 15:24:53 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,7 @@
+public class HelloGuys{
+             public static void main(String[] args) 
+             {
+                System.out.println("Hey Ram ");
+                
+             }
+             }
